68d501d735965e13a96b11940dfe268dc7044345
慕晚 <117713200+dm192@users.noreply.github.com>
2024-08-02 20:02:08 +0800

    Update make.py

--- make/make.py
+++ make/make.py
@@ -2,17 +2,17 @@
 import os
 import sys
 
-print ("- Make Started")
-print ("- Check File : ./main.cpp")
-if (os.path.isfile("../main.cpp") == True):
-  print ("- Check File Done : ./main.cpp")
+print("- Make Started")
+print("- Check File : ./main.cpp")
+if(os.path.isfile("../main.cpp") == True):
+  print("- Check File Done : ./main.cpp")
   os.system("g++ ../main.cpp && mv ../main.out ../out/main" && chmod +x ../out/main)
-  print ("./main.cpp -> ./out/main")
+  print("./main.cpp -> ./out/main")
   
   # More File
   
-  print ("- Make Success")
+  print("- Make Success")
   sys.exit(0)
 else:
-  print ("- Check File Fail : ./main.cpp")
+  print("- Check File Fail : ./main.cpp")
   sys.exit(1)
