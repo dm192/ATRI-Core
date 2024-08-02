f7e36d212f8ecc025141b8c86d25d6285020f9f3
慕晚 <117713200+dm192@users.noreply.github.com>
2024-08-02 21:11:38 +0800

    Update make.py

--- make/make.py
+++ make/make.py
@@ -3,11 +3,17 @@ import os
 import sys
 
 print(" * Make Started")
+if os.path.exists("./out/") == False:
+  print(" * Dir ./out/ not found , Creating...")
+  os.system("mkdir out")
+  if os.path.exists("./out/") == False:
+    print(" * Dir ./out/ create failed")
+    sys.exit(1)
 print(" * Check File : ./main.cpp")
 #if后未加空格;无需==True
 if os.path.isfile("./main.cpp"):
   print(" * Check File Done : ./main.cpp")
-  os.system("g++ ./main.cpp && mv ./main.out ./out/main && chmod +x ./out/main")
+  os.system("g++ ./main.cpp && mv ./a.out ./out/main && chmod +x ./out/main")
   print("./main.cpp -> ./out/main")
   
   # More File
