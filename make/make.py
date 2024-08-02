a08bfb27956b6ae1755578f98b5ef527f5343b51
Onesoft <onesoftqwq@163.com>
2024-08-02 20:40:48 +0800

    Update make.py

--- make/make.py
+++ make/make.py
@@ -5,9 +5,9 @@ import sys
 print(" * Make Started")
 print(" * Check File : ./main.cpp")
 #if后未加空格;无需==True
-if os.path.isfile("../main.cpp"):
+if os.path.isfile("./main.cpp"):
   print(" * Check File Done : ./main.cpp")
-  os.system("g++ ../main.cpp && mv ../main.out ../out/main && chmod +x ../out/main")
+  os.system("g++ ./main.cpp && mv ./main.out ./out/main && chmod +x ./out/main")
   print("./main.cpp -> ./out/main")
   
   # More File
